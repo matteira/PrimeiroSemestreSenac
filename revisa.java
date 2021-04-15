2f88fc7f5221b0d2c67704fee74b39f050c12063
Gabriel Mattos <gabrielmattoscosta4@gmail.com>
2021-04-14 22:08:49 -0300

    Revisão Stelvio

---
+++ revisa.java
@@ -0,0 +1,58 @@
+package Estudos;
+
+import java.util.Scanner;
+
+/*public class revisa {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Escreva um número: ");
+        char numero = leitor.nextLine().charAt(0);
+        
+
+        if (numero <= 0){
+            System.out.println("Esse número é positivo.");
+        } else if (numero > 0){
+            System.out.println("Esse número é negativo.");
+        }
+    }
+    
+}*/
+
+/*public class revisa {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digita o raio da esfera: ");
+
+        double p, r, El, V, res;
+        p = 3.14;
+        r = leitor.nextDouble();
+        El = Math.pow(r, 3);
+        V = 4 * p * El / 3;
+        res = V;
+
+        System.out.println("Esse é o raio da esfera " + res);
+
+        double a, b, q, resu;
+        System.out.print("Digita a área do quadrado: ");
+        
+        b = leitor.nextDouble();
+        q = leitor.nextDouble();
+        a = b * q;
+        resu = a;
+
+        
+        System.out.println("Essa é a área do quadrado " + resu);
+ 
+        if (res > resu){
+            System.out.println("O raio da esfera é maior que o quadrado ");
+        } else {
+            System.out.println("A área do quadrado é maior que o raio da esfera ");
+        }
+    }
+}*/
+
+public class revisa{
+    public static void main(String[] args) {
+        
+    }
+}
