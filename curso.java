bc5ee7cf1c1ae38c2a14a32e2cdcd6dfe26f8b01
Gabriel Mattos <gabrielmattoscosta4@gmail.com>
2021-04-14 21:22:39 -0300

    Curso por fora feito no udemy

---
+++ curso.java
@@ -0,0 +1,97 @@
+package Curso_udemy;
+
+import java.util.Scanner;
+
+/*public class curso {
+    public static void main(String[] args) {
+
+     Scanner leitor = new Scanner(System.in);
+
+     System.out.print("Adiciona um o numero 1: ");
+    double _1numero = leitor.nextFloat();
+
+    System.out.print("Adiciona o numero 2: ");
+    double _2numero = leitor.nextFloat();
+
+    double soma = _1numero + _2numero;
+    double sub = _1numero - _2numero;
+    double multi = _1numero * _2numero;
+    double div = _1numero / _2numero;
+
+        System.out.println("Esse é a soma " + soma + ".");
+        System.out.printf("Arredondando a soma = %.2f%n", soma);
+        System.out.println("");
+
+        System.out.println("Esse é a subtração " + sub + ".");
+        System.out.printf("Arredondando a subtração = %.2f%n", sub);
+        System.out.println("");
+
+        System.out.println("Esse é a multiplicação " + multi + ".");
+        System.out.printf("Arredondando a multiplicação = %.2f%n", multi);
+        System.out.println("");
+
+        System.out.println("Esse é a divisão " + div + ".");
+        System.out.printf("Arredondando a divisão = %.2f%n", div);
+        System.out.println("");
+        
+    }
+}*/
+
+/*public class curso{
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu nome abaixo: ");
+        String nome = leitor.next();
+        
+    }
+}*/
+
+
+/*public class curso{
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu nome abaixo: ");
+        char nome = leitor.next().charAt(0);
+        
+    }
+}*/
+
+/*public class curso{
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu dados abaixo: ");
+        String nome, genero;
+        nome = leitor.nextLine();
+        genero = leitor.nextLine();
+
+        System.out.println("Muito obrigado");
+
+    }
+}*/
+
+/*public class curso{
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu dados abaixo: ");
+        System.out.println();
+
+        char nome, idade, genero;
+
+        System.out.print("Nome: ");
+        nome = leitor.nextLine().charAt(0);
+
+        System.out.print("Idade: ");
+        idade = leitor.nextLine().charAt(0);
+
+        System.out.print("Genêro: ");
+        genero = leitor.nextLine().charAt(0);
+
+        System.out.println();
+        System.out.println("Muito obrigado");
+
+    }
+}*/
+
+
